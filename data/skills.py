f34884bd8a3c727cce4a91fb393aac3662ece1c8
Evotushon <danielspa2019@gmail.com>
2021-09-30 17:46:36 +0200

    Added some things for the slots

--- data/skills.py
+++ data/skills.py
@@ -1,11 +1,13 @@
 from dataimports import *
 class skill:
-    def __init__(self, skillname, maxlevel, gunneronly, swordmasteronly, foreveryweapon):
+    def __init__(self, skillname, maxlevel, gunneronly, swordmasteronly, foreveryweapon, jewelname, jewelslots):
         self.skillname = str(skillname)
         self.maxlevel = int(maxlevel)
         self.gunneronly = bool(gunneronly)
         self.swordmasteronly = bool(swordmasteronly)
         self.foreveryweapon = foreveryweapon
+        self.jewelname = str(jewelname)
+        self.jewelslots = int(jewelslots)
         if self.foreveryweapon:
             swordmasteronly = False
             gunneronly = False
