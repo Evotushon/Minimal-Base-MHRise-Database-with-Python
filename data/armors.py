f8aa98175fe3b8f973c10cb65559cf1884e58dff
Evotushon <danielspa2019@gmail.com>
2021-09-30 11:15:53 +0200

    added a little thing

--- data/armors.py
+++ data/armors.py
@@ -1,8 +1,9 @@
 from dataimports import *
 class armor:
-    def __init__(self, setname, monster, requireditems, armorpieces, skills):
+    def __init__(self, setname, monster, requireditems, armorpieces, skills, skill_level):
         self.setname = str(setname)
         self.monster = monster
         self.requireditems = dict(requireditems)
         self.armorpieces = dict(armorpieces)
         self.skills = dict(skills)
+        self.skill_level = int(skill_level)
